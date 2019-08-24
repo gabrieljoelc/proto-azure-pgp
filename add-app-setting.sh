26384e95f426c236de719d3dab2a84b94161fcac
Gabriel Chaney <gabriel.chaney@gmail.com>
2019-08-24 15:52:49 -0700

    Remove app setting script
    Since we can use terraform apply

--- add-app-setting.sh
+++
@@ -1,2 +0,0 @@
-# from https://docs.microsoft.com/en-us/azure/app-service/app-service-web-ssl-cert-load#make-the-certificate-accessible
-az webapp config appsettings set --name $AZURE_APP_SERVICE --resource-group $AZURE_RESOURCE_GROUP --settings WEBSITE_LOAD_CERTIFICATES=$WEBSITE_LOAD_CERTIFICATES
